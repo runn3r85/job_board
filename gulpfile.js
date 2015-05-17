d486be8e8c16586feea33b619a88369621d8333f
Brandon Barrette <brandon.barrette@gmail.com>
2015-05-17 16:19:14 -0700

    setup bower, gulp, bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 gulp.task('js', function(){
     browserify('./public/javascripts/src/app.jsx')
@@ -12,8 +14,28 @@ gulp.task('js', function(){
         .pipe(gulp.dest('public/javascripts/build/'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
+gulp.task('sass', function() {
+    gulp.src('public/stylesheets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/stylesheets/css'));
 })
 
-gulp.task('default', ['js', 'watch']);
+
+gulp.task('watch', function() {
+    gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
+    gulp.watch("public/stylesheets/scss/*.scss", ["sass"]);
+})
+
+gulp.task('default', ['js', 'sass', 'watch'], function(){
+  nodemon({
+    script: './bin/www',
+    ext: 'js html',
+    env: {
+      PORT: 3000
+    },
+    ignore: ["README.md", "node_modules/**", ".DS_Store", ".gitignore"]
+  })
+  .on('restart', function(){
+    console.log('Restarting');
+  });
+});
--- public/javascripts/src/app.jsx
+++ public/javascripts/src/app.jsx
@@ -1,3 +1,6 @@
+var $ = jQuery = require('../../libraries/jquery/dist/jquery');
+var bootstrap = require('../../libraries/bootstrap-sass-official/assets/javascripts/bootstrap');
+
 var React = require('react');
 var HelloWorld = require('./HelloWorld.jsx');
 
